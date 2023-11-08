df4d6daf772706e7785bc3d889207b27fcb6af76
UMMAN <ummanmemmedov2005@gmail.com>
2023-11-08 23:31:19 +0400

    with url

--- main.go
+++ main.go
@@ -12,4 +12,5 @@ func main() {
 	word4 := packet.WhatIsYourVersion()
 	word5 := "That is finish"
 	fmt.Println(word, "\n", word2, "\n", word3, "\n", word4, "\n", word5)
+	// https://www.reddit.com/r/golang/comments/pqx5tu/am_i_using_go_get_latest_correctly/
 }
