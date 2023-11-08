c93c91ea2bfe3a84f56c7ca702e7536938394972
UMMAN <ummanmemmedov2005@gmail.com>
2023-11-08 23:15:07 +0400

    version..7

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/UMMAN2005/golang_course
+
+go 1.21.4
+
+require github.com/UMMAN2005/packet v1.1.0
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package golang_couse
+package main
+
+import (
+	"fmt"
+	"github.com/UMMAN2005/packet"
+)
+
+func main() {
+	word := packet.Hello()
+	word2 := packet.Hi()
+	word3 := packet.How_are_you()
+	fmt.Println(word, "\n", word2, "\n", word3)
+}
